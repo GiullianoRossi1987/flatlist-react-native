2e3f2e237dde9f3f1562a30bb5da764ed7d0828e
GiullianoRossi1987 <giulliano.scatalon.rossi@gmail.com>
2021-08-26 16:18:29 -0300

    Função basica feita

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SafeAreaView, Image} from 'react-native';
+
+
+const DATA = [
+	{
+		"id": 1,
+		"name_curso": "Analise e Desenvolvimento de Sistemas",
+		"desc": "Curso sobre computadores",
+		"img": "https://programming-examples.com/wp-content/uploads/sites/3/2018/08/chris-ried-512801-unsplash-300x200.jpg"
+	},
+	{
+		"id": 2,
+		"name_curso": "Administracao",
+		"desc": "Curso sobre empresas",
+		"img": "https://cdn.univicosa.com.br/img/portal/noticia/img/adm_7.jpg"
+	},
+	{
+		"id": 3,
+		"name_curso": "Desenvolvimento de Jogos",
+		"desc": "Curso sobre computadores e jogos",
+		"img": "https://warpzone.me/wp-content/uploads/2020/04/th-e1586376486510-768x432.png"
+	},
+];
+
+const Item = ({ name_curso, desc, img }) => (
+	<View style={styles.item}>
+		<Image style={styles.img} source={{uri: img}} />
+		<Text style={styles.textItem}>Curso: {name_curso}</Text>
+		<Text style={styles.textItem}>Descrição: {desc}</Text>
+	</View>
+);
+
+
 
 export default function App() {
+  const renderItem = ({ item }) => (
+	  <Item name_curso={item.name_curso} desc={item.desc} img={item.img} />
+  );
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+	  data={DATA}
+	  renderItem={renderItem}
+	  keyExtractor={item => item.id}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +57,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  item: {
+	  backgroundColor: "blue",
+	  padding: 10,
+	  marginBottom: 20,
+	  flexDirection: "row"
+  },
+  textItem: {
+	  backgroundColor: "red",
+	  alignSelf: "flex-start"
+  },
+  img: {
+	  border: 10,
+	  width: 200,
+	  height: 200,
+	  alignSelf: "flex-end",
+	  textAlign: "center"
+  }
 });
